4fc8d7c6857b7a179e2a690f643054fee7b254ec
shubham6347 <134758141+shubham6347@users.noreply.github.com>
2024-08-08 00:06:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 # Configure the AWS Provider
 provider "aws" {
-  region     = "ap-south-1"
-  
+  region = "ap-south-1"
 }
 
 # Create an IAM Role
@@ -39,13 +38,38 @@ resource "aws_subnet" "terraform" {
   availability_zone = "ap-south-1a"
 }
 
-# Create an EC2 Instance
-resource "aws_instance" "terraform" {
-  ami           = "ami-0f58b397bc5c1f2e8"  # Replace with your desired AMI ID
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.terraform.id
-  tags = {
-    Name = "terraform-instance"
+# Create a Security Group
+resource "aws_security_group" "instance" {
+  vpc_id = aws_vpc.appvpc.id
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create a Launch Configuration
+resource "aws_launch_configuration" "terraform" {
+  name                  = "terraform-launch-configuration"
+  image_id              = "ami-0f58b397bc5c1f2e8"  # Replace with your desired AMI ID
+  instance_type         = "t2.micro"
+  iam_instance_profile  = aws_iam_instance_profile.ec2_instance_profile.name
+  security_groups       = [aws_security_group.instance.id]
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
@@ -56,12 +80,37 @@ resource "aws_autoscaling_group" "terraform" {
   desired_capacity          = 1
   launch_configuration      = aws_launch_configuration.terraform.name
   vpc_zone_identifier       = [aws_subnet.terraform.id]
+  tag {
+    key                 = "Name"
+    value               = "terraform-instance"
+    propagate_at_launch = true
+  }
 }
 
-# Create a Launch Configuration
-resource "aws_launch_configuration" "terraform" {
-  name          = "terraform-launch-configuration"
-  image_id      = "ami-0f58b397bc5c1f2e8"  # Replace with your desired AMI ID
-  instance_type = "t2.micro"
+# Create an IAM Instance Profile
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "ec2_instance_profile"
+  role = aws_iam_role.ec2_role.name
 }
 
+# Output the public IP address of the first instance in the Auto Scaling Group
+output "instance_ip" {
+  value = aws_instance.terraform.public_ip
+}
+
+# Create an EC2 Instance
+resource "aws_instance" "terraform" {
+  ami                    = "ami-0f58b397bc5c1f2e8"  # Replace with your desired AMI ID
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.terraform.id
+  iam_instance_profile   = aws_iam_instance_profile.ec2_instance_profile.name
+  security_groups        = [aws_security_group.instance.name]
+  tags = {
+    Name = "terraform-instance"
+  }
+}
+
+# Output the instance IP address
+output "instance_ip" {
+  value = aws_instance.terraform.public_ip
+}
